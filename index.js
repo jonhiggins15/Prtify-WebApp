e966f861ff7dfcec6251a5df886691f3d7866a25
bseibo61 <brseibol@buffalo.edu>
2017-11-04 14:26:47 -0400

    annon auth is set up

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    alert("user signed in");
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+  } else {
+    alert("signed out");
+  }
+});
