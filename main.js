58ec49916912f2b0e2a6d55ff0ab06b08b2ff115
bseibo61 <brseibol@buffalo.edu>
2017-11-04 18:25:23 -0400

    auth in main.js

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
-var app = angular.module('main',['firebase']);
-
-app.controller("testController", function($scope, $firebaseObject){
-  var ref = firebase.database().ref();
-  var obj = $firebaseObject(ref);
-  console.log(obj);
+firebase.auth().onAuthStateChanged(function(user) {
+  var isAnonymous = user.isAnonymous;
+  uid = user.uid;
+  var json = getJson();
+  found = false;
+  var room;
+  for (var u in json.users) {
+    if (u == uid) {
+      room = u.room;
+      alert(room);
+      found = true;
+    }
+  }
+  if(!found){
+    window.location.assign('index.html');
+  }
 });
+
+//returns a JSON of the database using REST API
+function getJson() {
+  var xhttp = new XMLHttpRequest();
+
+  //TODO: IMPORTANT: before putting this on the website, change rules and put some
+  //form of authentication in the url
+  xhttp.open("GET", "https://voteify.firebaseio.com/.json?print=pretty", false);
+  xhttp.send();
+  var response = JSON.parse(xhttp.responseText);
+  return response;
+}
