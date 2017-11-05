c594ecf30d8e60a11f709610116237bf6b6af5f0
bseibo61 <brseibol@buffalo.edu>
2017-11-04 22:22:26 -0400

    sorting song queue

--- main.js
+++ main.js
@@ -1,20 +1,59 @@
+var authDone = false;
+var party;
+
 firebase.auth().onAuthStateChanged(function(user) {
   var isAnonymous = user.isAnonymous;
   uid = user.uid;
   var json = getJson();
   found = false;
-  var room;
   for (var u in json.users) {
     if (u == uid) {
-      room = u.room;
-      alert(room);
+      party = json.users[u].party;
       found = true;
     }
   }
-  if(!found){
-    window.location.assign('index.html');
-  }
+  // if (!found) {
+  //   window.location.assign('index.html');
+  // }
+  authDone = true;
 });
+
+
+setTimeout(function(){
+  var partyRef = firebase.database().ref('parties/' + party + '/queue');
+  partyRef.on('value', function(snapshot) {
+    var info = snapshot.val();
+    var songList = []
+    for(var song in info){
+      info[song].total = info[song].upvotes - info[song].downvotes;
+      songList.push(info[song]);
+    }
+    console.log(songList);
+    songList.sort();
+    songList.sort(function(a, b){
+      return b.total - a.total;
+    });
+    console.log(songList);
+  });
+}, 500);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //returns a JSON of the database using REST API
 function getJson() {
