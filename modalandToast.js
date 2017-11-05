da8aedc32fa5a27beb592e3bb926cf28cf1a12da
Satya <satyakra@buffalo.edu>
2017-11-05 05:09:57 -0500

    Modal and Toast js.

---
+++ modalandToast.js
@@ -0,0 +1,20 @@
+/**
+ * Created by satya on 11/5/17.
+ */
+function modalClicked() {
+$('#modal1').modal('open');
+}
+
+function toastClicked() {
+    var $toastContent = $('<span>New Song Added!</span>').add($('<button class="btn-flat toast-action" onclick="scrollToBottom()">Scroll to Bottom</button>'));
+    Materialize.toast($toastContent, 4000, 'rounded');
+}
+
+function scrollToBottom() {
+    var toastElement = $('.toast').first()[0];
+    var toastInstance = toastElement.M_Toast;
+    toastInstance.remove();
+    window.scrollTo(0,document.body.scrollHeight);
+}
+
+$('.modal').modal();
